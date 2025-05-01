8913e1a01fe6442324e84907cf146fc332098f21
Fernando Calvino Balonero <fercbrt@gmail.com>
2025-05-01 11:29:10 +0200

    Created initial configuration

---
+++ src/test/java/es/fercbrt/mamadin/MamadinApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fercbrt.mamadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamadinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
